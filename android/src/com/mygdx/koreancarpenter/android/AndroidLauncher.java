d6a1ae1c94470d0213a09651a33f653278562938
NakOh <kk0704@naver.com>
2015-10-30 11:29:35 +0900

    MakeProject

---
+++ android/src/com/mygdx/koreancarpenter/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.koreancarpenter.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.koreancarpenter.KoreanCarpenter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KoreanCarpenter(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/koreancarpenter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.koreancarpenter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.koreancarpenter.KoreanCarpenter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KoreanCarpenter(), config);
+	}
+}
