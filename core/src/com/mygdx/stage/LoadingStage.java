42507a77beb50ae5ff2607b5364aa9100439f910
NakOh <kk0704@naver.com>
2015-11-06 13:00:13 +0900

    #6 자동 리소스 코드 추가

--- core/src/com/mygdx/stage/LoadingStage.java
+++ core/src/com/mygdx/stage/LoadingStage.java
@@ -1,13 +1,11 @@
 package com.mygdx.stage;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.mygdx.manager.AssetManager;
 
 public class LoadingStage extends Stage {
-	private AssetManager assetManager;
 
 	public void makeStage() {
-		assetManager.getInstance();
+
 	}
 
 }
