3420c3459ee8c42e617de55a87950c939a7e9c42
NakOh <kk0704@naver.com>
2015-11-13 15:57:06 +0900

    #2 게임 화면 구성 어느정도 완료

--- core/src/com/mygdx/stage/GameStage.java
+++ core/src/com/mygdx/stage/GameStage.java
@@ -8,14 +8,22 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class GameStage extends Stage {
 	private final String tag = "GAME_STAGE";
+	// Table
 	private Table levelTable;
 	private Table bottomTable;
+	private Table gameTable;
+	private Table treeTable;
+
+	// 임시 Button, 실제 사용할 UI로 대체
 	private TextButton axButton;
 	private TextButton gloveButton;
 	private TextButton wagonButton;
 	private TextButton bagButton;
 	private TextButton itemButton;
 	private TextButton endingButton;
+	private TextButton treeButton;
+	private TextButton sellButton;
+
 	private Skin skin;
 	private int x, y;
 
@@ -23,11 +31,15 @@ public class GameStage extends Stage {
 		x = this.getViewport().getScreenWidth();
 		y = this.getViewport().getScreenHeight();
 
+		gameTable = makeTable("game");
 		bottomTable = makeTable("bottom");
 		levelTable = makeTable("level");
+		treeTable = makeTable("tree");
 
 		addActor(bottomTable);
 		addActor(levelTable);
+		addActor(gameTable);
+		addActor(treeTable);
 		return this;
 	}
 
@@ -40,11 +52,11 @@ public class GameStage extends Stage {
 			wagonButton = new TextButton("수레버튼", skin);
 			table.setFillParent(true);
 			table.bottom();
-			table.add(axButton).size(300f, 100f);
+			table.add(axButton).size(x / 6, 100f);
 			table.row();
-			table.add(gloveButton).size(300f, 100f);
+			table.add(gloveButton).size(x / 6, 100f);
 			table.row();
-			table.add(wagonButton).size(300f, 100f);
+			table.add(wagonButton).size(x / 6, 100f);
 			table.padBottom(100f);
 			return table;
 		} else if (tableName.equals("bottom")) {
@@ -57,6 +69,21 @@ public class GameStage extends Stage {
 			table.add(bagButton).size(x / 3, 100f);
 			table.add(itemButton).size(x / 3, 100f);
 			table.add(endingButton).size(x / 3, 100f);
+			return table;
+		} else if (tableName.equals("game")) {
+			Table table = new Table();
+			table.setFillParent(true);
+			treeButton = new TextButton("이것은 나무", skin);
+			table.top();
+			table.add(treeButton).size(x, 400f);
+			return table;
+		} else if (tableName.equals("tree")) {
+			Table table = new Table();
+			table.setFillParent(true);
+			sellButton = new TextButton("나무 판매", skin);
+			table.top();
+			table.add(sellButton);
+			table.padTop(400f);
 			return table;
 		}
 		return null;
