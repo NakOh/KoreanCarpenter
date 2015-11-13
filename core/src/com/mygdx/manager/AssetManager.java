3be5fa4508f7a29fa3368fa1d499e9b6d96b732d
NakOh <kk0704@naver.com>
2015-11-13 11:07:59 +0900

    얼마 안되는 리소스를 위한 자동 소스를 구현하는 것은 비효율적이라 판단함

--- core/src/com/mygdx/manager/AssetManager.java
+++ core/src/com/mygdx/manager/AssetManager.java
@@ -1,31 +1,13 @@
 package com.mygdx.manager;
 
-import java.util.Map;
-
-import com.badlogic.gdx.Application.ApplicationType;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Json;
-
 public class AssetManager {
 	private static com.badlogic.gdx.assets.AssetManager instance;
-	private Map<String, String> filePathMap;
 
 	public static com.badlogic.gdx.assets.AssetManager getInstance() {
 		if (null == instance) {
 			instance = new com.badlogic.gdx.assets.AssetManager();
 		}
 		return instance;
-	}
-
-	public static void loadTexture() {
-		FileHandle file = Gdx.files.local("textureMap.json");
-		Json json = new Json();
-
-		if (Gdx.app.getType() != ApplicationType.Android) {
-			directoryTextureMapperRecursive(textureMap, Gdx.files.internal("./bin/texture"));
-			file.writeString(json.prettyPrint(textureMap), false);
-		}
 	}
 
 }
